d3c046edf1dcaad38ff3cb0a7dcfb937eb7622c1
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2018-10-13 00:48:40 +0100

    changes

--- Aula1/Xadrez/index.js
+++ Aula1/Xadrez/index.js
@@ -8,25 +8,21 @@ window.onload = function () {
     let cor1 = prompt("Indique qual primeira cor (hexadecimal):")
     let cor2 = prompt("Indique qual segunda cor (hexadecimal):")
 
-    canvas.height = lado*numeroQuadrados
-    canvas.width = lado*numeroQuadrados
-    canvas.style.border = "1px solid " + cor1
+    canvas.height = lado * numeroQuadrados
+    canvas.width = lado * numeroQuadrados
+    canvas.style.border = "25px solid " + cor1
     context.fillStyle = cor1
     context.fillRect(0, 0, lado * numeroQuadrados, lado * numeroQuadrados)
 
     for (let i = 0; i < numeroQuadrados; i++) {
         for (let j = 0; j < numeroQuadrados; j++) {
-            if (i % 2 === 0) {
-                if (j % 2 === 0) {
-                    context.fillStyle = cor2
-                    context.fillRect(lado * i, lado * j, lado, lado)
-                }
-            } else {
-                if (j % 2 !== 0) {
-                    context.fillStyle = cor2
-                    context.fillRect(lado * j, lado * i, lado, lado)
-                }
-                
+            if (i % 2 === 0 && j % 2 === 0) {
+                context.fillStyle = cor2
+                context.fillRect(lado * i, lado * j, lado, lado)
+            } else if (i % 2 !== 0 && j % 2 !== 0) {
+                context.fillStyle = cor2
+                context.fillRect(lado * j, lado * i, lado, lado)
+
             }
         }
     }
